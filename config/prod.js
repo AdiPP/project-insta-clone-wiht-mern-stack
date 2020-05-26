3fc8c5dea5e7a16776407964908463081b0a88bd
peps <adiputrapermana@gmail.com>
2020-05-26 21:31:29 +0700

    fixing env bug

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SEC,
   MAILGUN_API: process.env.MAILGUN_API,
   MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
-  EMAIL: process.env.MAILGUN_DOMAIN
+  EMAIL: process.env.EMAIL
 }
